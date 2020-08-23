742091eb37c96a495a736ac5e1a311f29ca5a764
makadown <maseflores79@gmail.com>
2020-08-23 14:19:12 -0500

    metiendo loader

--- script.js
+++ script.js
@@ -3,11 +3,26 @@ const quoteText = document.getElementById('quote');
 const authorText = document.getElementById('author');
 const twitterBtn = document.getElementById('twitter');
 const newQuoteBtn = document.getElementById('new-quote');
+const loader = document.getElementById('loader');
+
+// Show loading
+function loading() {
+    loader.hidden = false;
+    quoteContainer.hidden = true;
+}
+
+function complete() {
+    if (!loader.hidden) {
+        loader.hidden = true;
+        quoteContainer.hidden = false;
+    }
+}
 
 /**
  * Método para obtener cita del API
  */
 async function getQuote() {
+    loading();
     // const proxyUrl = 'https://cors-anywhere.herokuapp.com/';
     // Mi propio server n.n
     const proxyUrl = 'https://stormy-savannah-35034.herokuapp.com/';
@@ -17,21 +32,22 @@ async function getQuote() {
         const response = await fetch(proxyUrl + apiUrl);
         const data = await response.json();
         // si el autor esta en blanco, ponerlo unknown
-        if (data.quoteAuthor === '' ) {
+        if (data.quoteAuthor === '') {
             authorText.innerText = 'Unknown';
         } else {
             authorText.innerText = data.quoteAuthor;
         }
         // Reducir tamaño de fuente para citas muy largas.
-        if(data.quoteText.length > 50) {
+        if (data.quoteText.length > 50) {
             quoteText.classList.add('long-quote');
         } else {
             quoteText.classList.remove('long-quote');
         }
         quoteText.innerText = data.quoteText;
+        complete();
     } catch (error) {
-       // getQuote();
-       console.log(error);
+        // getQuote();
+        console.log(error);
     }
 }
 
