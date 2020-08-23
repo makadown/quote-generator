ec4748b329e3ba9f92803a4d3f6f1f7ed3cff28a
makadown <maseflores79@gmail.com>
2020-08-23 13:13:42 -0500

    agregando funcionalidad a botones

--- README.md
+++ README.md
@@ -4,9 +4,20 @@ Ejercicio 1 de curso de Javascript portfolio
 
 ## Sitios de apoyo para este proyecto
 
-
 Para customizar nuestro propio background 
+```
 http://www.heropatterns.com/
+```
 
 para las citas
+```
 http://forismatic.com/en/api
+```
+
+Para Twitter
+```
+https://developer.twitter.com/en/docs/twitter-for-websites/tweet-button/overview
+https://twitter.com/intent/tweet
+```
+
+
--- script.js
+++ script.js
@@ -1,15 +1,53 @@
-// Get Quote From API
+const quoteContainer = document.getElementById('quote-container');
+const quoteText = document.getElementById('quote');
+const authorText = document.getElementById('author');
+const twitterBtn = document.getElementById('twitter');
+const newQuoteBtn = document.getElementById('new-quote');
+
+/**
+ * Método para obtener cita del API
+ */
 async function getQuote() {
-    const proxyUrl = 'https://cors-anywhere.herokuapp.com/';
+    // const proxyUrl = 'https://cors-anywhere.herokuapp.com/';
+    // Mi propio server n.n
+    const proxyUrl = 'https://stormy-savannah-35034.herokuapp.com/';
     const apiUrl = 'http://api.forismatic.com/api/1.0/?method=getQuote&lang=en&format=json';
+
     try {
         const response = await fetch(proxyUrl + apiUrl);
         const data = await response.json();
-        console.log(data);
+        // si el autor esta en blanco, ponerlo unknown
+        if (data.quoteAuthor === '' ) {
+            authorText.innerText = 'Unknown';
+        } else {
+            authorText.innerText = data.quoteAuthor;
+        }
+        // Reducir tamaño de fuente para citas muy largas.
+        if(data.quoteText.length > 50) {
+            quoteText.classList.add('long-quote');
+        } else {
+            quoteText.classList.remove('long-quote');
+        }
+        quoteText.innerText = data.quoteText;
     } catch (error) {
-        console.log('whoops! no quote!', error);
+       // getQuote();
+       console.log(error);
     }
 }
+
+/**
+ * Método para emitir tweet
+ */
+function tweetQuote() {
+    const quote = quoteText.innerText;
+    const author = authorText.innerText;
+    const twitterUrl = `https://twitter.com/intent/tweet?text=${quote}-${author}`;
+    window.open(twitterUrl, '_blank');
+}
+
+// Escuchadores de eventos
+newQuoteBtn.addEventListener('click', getQuote);
+twitterBtn.addEventListener('click', tweetQuote);
 
 // On Load
 getQuote();
